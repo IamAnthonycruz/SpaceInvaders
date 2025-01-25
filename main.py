8340cd15f4372f4186cc9578e45ca6944b60249d
IamAnthonycruz <Charliecruz1224@gmail.com>
2025-01-25 08:58:23 -0500

    laser logic

--- main.py
+++ main.py
@@ -22,7 +22,22 @@ YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"
 # Background
 BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
 
+class Laser:
+    def __init__(self, x, y, img):
+        self.x = x
+        self.y = y
+        self.img = img
+        self.mask = pygame.mask.from_surface(self.img)
+    def draw(self, window):
+        window.blit(self.img, (self.x, self.y))
+    def move(self, vel):     
+        self.y += vel   
+    def off_screen(self, height):
+        return self.y <= height and self.y >= 0
+    def collision(self, obj): 
+        return collide(obj, self)       
 class Ship:
+    COOLDOWN = 30
     def __init__(self,x,y,health=100):
         self.x = x
         self.y = y
@@ -34,7 +49,16 @@ class Ship:
 
     def draw(self, window):
         window.blit(self.player_img, (self.x, self.y))
-
+    def cool_down(self):
+        if self.cool_down_counter >= self.COOLDOWN:
+            self.cool_down_counter = 0
+        elif self.cool_down_counter > 0:
+            self.cool_down_counter += 1
+    def shoot(self):
+        if self.cool_down_counter == 0:
+            laser = Laser(self.x, self.y, self.laser_img)
+            self.lasers.append(laser)
+            self.cool_down_counter = 1
     def get_width(self):
         return self.player_img.get_width()
     def get_height(self):
@@ -59,7 +83,10 @@ class Enemy(Ship):
         self.mask = pygame.mask.from_surface(self.ship_img)
     def move(self, vel):
         self.y += vel
-        
+def collide(obj1, obj2):
+    offset_x = obj2.x - obj1.x
+    offset_y = obj2.y - obj1.y
+    return obj1.mask.overlap(obj2.mask, (offset_x, offset_y)) != None
 def main():   
     run = True
     FPS = 60
@@ -93,6 +120,7 @@ def main():
 
     while run:
         clock.tick(FPS)
+        redraw_window()
         if lives <=0 or player.health <= 0:
             lost = True
             loss_count += 1
@@ -126,6 +154,6 @@ def main():
             if enemy.y + enemy.get_height() > HEIGHT:
                 lives -= 1
                 enemies.remove(enemy)
-        redraw_window()
+        
 
 main()
